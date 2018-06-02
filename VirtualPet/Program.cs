045405d9d816be84cb9d511492366671d7c3312a
malachilee <malachilee6@gmail.com>
2018-06-02 18:02:14 -0400

    fields, props, methods

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Virtual_Pet pablo = new Virtual_Pet(5, 25, "pablo");
+
+         // Test: Console.WriteLine("" + pablo.Age + "" + pablo.Hunger + "" + pablo.Name);
+        }
+    }
+}
---
+++ VirtualPet/Virtual Pet.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Virtual_Pet
+    {
+     // Declare Fields
+
+        private int age;
+        private int hunger;
+        private int thrist;
+        private string name;
+
+     // Declare Properties
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Thrist
+        {
+            get { return this.thrist; }
+            set { this.thrist = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+     // Declare Constructors
+
+        public Virtual_Pet()
+        {
+         // Default Constructor
+        }
+
+        public Virtual_Pet(int age, int hunger, int thrist, string name)
+        {
+            this.Age = age;
+            this.Hunger = hunger;
+            this.Name = name;
+        }
+
+     // Declare Methods
+
+        public void Tick()
+        {
+            Age++;
+            if(Age > 20)
+            {
+                Console.WriteLine("Your pet died from old age.");
+            }
+
+            Hunger += 12;
+            if(Hunger < 0)
+            {
+                Console.WriteLine("Your pet died from starvation.");
+            }
+       
+        }
+
+        public void Feed()
+            {
+            this.Hunger -= 30;
+            this.Thrist += 20;
+            Console.WriteLine("{0} says thanks, that tasted great!", this.Name);
+            }
+
+        public void GiveWater()
+        {
+            this.Thrist -= 50;
+        }
+
+            
+
+
+
+
+
+
+
+
+
+
+    }
+}
